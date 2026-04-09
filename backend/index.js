542cfd1cbc8fbef0383ec8610a6f65dd92f82ce4
CarlosMaldonadoKMB <carlos.maldonado@kairosmb.org>
2026-04-08 23:17:44 -0400

    Backend configurado para base de datos en la nube

--- backend/index.js
+++ backend/index.js
@@ -34,12 +34,14 @@ const upload = multer({ storage: multer.memoryStorage() });
 // ==========================================
 // 1. CONFIGURACIÓN DE BASE DE DATOS
 // ==========================================
+// ==========================================
+// 1. CONFIGURACIÓN DE BASE DE DATOS (NUBE)
+// ==========================================
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // ==========================================
